5e0d6d044aa98ab906ccab74c4254c5bfcf2424d
Himesh <pandeyhimesh65@gmail.com>
2026-09-10 23:15:49 +0545

    Learning python through projects

--- script.py
+++ script.py
@@ -8,49 +8,57 @@
 
 # ATM SIMULATION
 
-# correct_pin = 1234
-# balance = 5000
-# attempt = 0
+correct_pin = 1234
+balance = 5000
+attempt = 0
 
-# while attempt < 3:
+while attempt < 3:
     
-#     Ipin = input('Enter PIN (x for exit): ')
-    
-#     if(Ipin == correct_pin):
-    
-#         while True:
-#               print("Enter your choice (1,2,3,4): ")
-#               print("1.Check Balance")
-#               print("2.Deposit")
-#               print("3.Withdraw")
-#               print("4.Exit")
-          
-#               choice = int(input(''))
-          
-#               match choice:
-#                   case 1:
-#                       print(f'Your balance: {balance}')
-#                   case 2:
-#                       deposit = int(input("Enter amount to deposit:"))
-#                       balance = balance + deposit 
-#                   case 3:
-#                       withdraw = int(input('Enter amount to withdraw: '))
-#                       if withdraw <= balance:
-#                         balance = balance - withdraw
-#                         print("Withdraw successful")
-#                       else:
-#                          print("Insufficient balance")      
-#                   case 4:
-#                       print('Exit')
-#                       break;
-#                   case _:
-#                       print("Invalid choice!")  
-#     elif Ipin == "x":
-#         break
-#     else:
-#         print("Incorrect PIN")
-#         attempt += 1
-#         print(f'Number of attempts: {attempt} (max:3) {3-attempt} left')
+    Ipin = input('Enter PIN (x for exit): ')
 
-# if attempt == 3:
-#     print("Your card is blocked")    
+    if Ipin == "x":
+        break
+    
+    elif(int(Ipin) == correct_pin):
+    
+        while True:
+              print("Enter your choice (1,2,3,4): ")
+              print("1.Check Balance")
+              print("2.Deposit")
+              print("3.Withdraw")
+              print("4.Exit")
+              choice = int(input('Your choice: '))
+          
+              match choice:
+                  case 1:
+                      print("------------------------")
+                      print(f'Your balance: {balance}')
+                      print("------------------------")
+                  case 2:
+                      deposit = int(input("Enter amount to deposit:"))
+                      balance = balance + deposit
+                      print("------------------------")
+                      print(f'Deposited Successfully\nYour new balance: {balance}')
+                      print("------------------------")
+                  case 3:
+                      withdraw = int(input('Enter amount to withdraw: '))
+                      if withdraw <= balance:
+                        balance = balance - withdraw
+                        print("Withdraw successful")
+                        print("------------------------")
+                        print(f'Your new balance: {balance}')
+                        print("------------------------")
+                      else:
+                         print("Insufficient balance")      
+                  case 4:
+                      print('Exit')
+                      break;
+                  case _:
+                      print("Invalid choice!")  
+    else:
+        print("Incorrect PIN")
+        attempt += 1
+        print(f'Number of attempts: {attempt} (max:3) {3-attempt} left')
+
+if attempt == 3:
+    print("Your card is blocked")    
